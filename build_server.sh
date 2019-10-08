cd8f1c10aad8ee1d3b13be4645e3288871f6a5ee
Heyward Fann <fannheyward@gmail.com>
2019-10-08 10:39:10 +0800

    feat(build): bundle build

--- build_server.sh
+++ build_server.sh
@@ -2,6 +2,8 @@
 
 set -ue
 
+ROOT=`pwd`
+
 tag=`curl https://api.github.com/repos/microsoft/pyright/releases/latest | grep tag_name | sed -E 's/.*"([^"]+)".*/\1/'`
 url="https://github.com/microsoft/pyright/archive/${tag}.tar.gz"
 
@@ -9,15 +11,13 @@ echo "Downloading pyright ${tag}"
 curl -L -O ${url}
 
 tar xzf ${tag}.tar.gz
-cd ./pyright-${tag}
-
-npm install && cd ./server && npm install && npm run build && cd ..
+cd ./pyright-${tag} && npm install && cd ./server && npm install && npm run build:serverProd
 
 test $? -ne 0 && echo "npm build error" && exit
 
 echo "Pyright build success!"
 
-cd ..
+cd ${ROOT}
 rm -rf server
 rm -rf typeshed-fallback
 
@@ -27,4 +27,4 @@ cp -R ./pyright-${tag}/client/typeshed-fallback .
 rm -rf ${tag}.tar.gz
 rm -rf ./pyright-${tag}
 
-find server -type f | grep -v "js$" | grep -v "json$" | xargs rm
+rm -rf ./server/node_modules ./server/server.bundle.js.map
