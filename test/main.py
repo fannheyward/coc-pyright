a65f660e3c491c0198ac48941916d560e709fc3a
Heyward Fann <fannheyward@gmail.com>
2020-05-11 09:54:10 +0800

    chore(test): update

--- test/main.py
+++ test/main.py
@@ -3,3 +3,14 @@
 from test import m
 
 m.greeting(1)
+
+class ClsWithCallFunc:
+    def __init__(self, *init_args, **init_kwargs):
+        print("INIT")
+
+    def __call__(self, *call_args, **call_kwargs):
+        print("CALL")
+
+if __name__ == '__main__':
+    a = ClsWithCallFunc()
+    a(3, 5)
