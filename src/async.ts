92c1c0f95158f825394d321abc07954d69dcd9a3
Heyward Fann <fannheyward@gmail.com>
2020-11-23 09:41:32 +0800

    fix: ts-ignore

--- src/async.ts
+++ src/async.ts
@@ -31,6 +31,7 @@ class DeferredImpl<T> implements Deferred<T> {
   private _promise: Promise<T>;
   constructor(private scope: any = null) {
     this._promise = new Promise<T>((res, rej) => {
+      // @ts-ignore
       this._resolve = res;
       this._reject = rej;
     });
