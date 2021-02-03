41008b280bdb4599139c84e520a47154ff77b0e6
Heyward Fann <fannheyward@gmail.com>
2021-02-03 13:02:59 +0800

    feat: improve signatureHelp
    close #293

--- src/index.ts
+++ src/index.ts
@@ -35,6 +35,9 @@ class PyrightExtensionFeature implements StaticFeature {
   fillClientCapabilities(capabilities: any) {
     // TODO: use DiagnosticTag.Deprecated
     capabilities.textDocument.publishDiagnostics.tagSupport = { valueSet: [2] };
+    // Pyright set activeParameter = -1 when activeParameterSupport enabled
+    // this will break signatureHelp
+    capabilities.textDocument.signatureHelp.signatureInformation.activeParameterSupport = false;
   }
 }
 
