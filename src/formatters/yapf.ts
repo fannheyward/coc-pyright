939ac609672216c44c91017e36644fb602c16f66
Heyward Fann <fannheyward@gmail.com>
2021-03-05 10:38:26 +0800

    refactor: improve

--- src/formatters/yapf.ts
+++ src/formatters/yapf.ts
@@ -8,14 +8,12 @@ export class YapfFormatter extends BaseFormatter {
   }
 
   public formatDocument(document: TextDocument, options: FormattingOptions, token: CancellationToken, range?: Range): Thenable<TextEdit[]> {
-    const formatSelection = range ? range : false;
-
     const yapfArgs = ['--diff'];
     if (this.pythonSettings.formatting.yapfArgs.length > 0) {
       yapfArgs.push(...this.pythonSettings.formatting.yapfArgs);
     }
-    if (formatSelection) {
-      yapfArgs.push(...['--lines', `${range!.start.line + 1}-${range!.end.line + 1}`]);
+    if (range) {
+      yapfArgs.push(...['--lines', `${range.start.line + 1}-${range.end.line + 1}`]);
     }
     // Yapf starts looking for config file starting from the file path.
     const fallbarFolder = this.getWorkspaceUri(document)?.fsPath;
