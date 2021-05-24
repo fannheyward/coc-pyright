231ce7df29805d5f1a56ab382010a41b6bd3421e
Heyward Fann <fannheyward@gmail.com>
2021-05-24 18:46:33 +0800

    feat: ignore test_*.py for pydocstyle by default
    close #494

--- src/linters/pydocstyle.ts
+++ src/linters/pydocstyle.ts
@@ -9,7 +9,7 @@ export class PyDocStyle extends BaseLinter {
   }
 
   protected async runLinter(document: TextDocument, cancellation: CancellationToken): Promise<ILintMessage[]> {
-    const messages = await this.run([Uri.parse(document.uri).fsPath], document, cancellation);
+    const messages = await this.run(["--match='(?!test_).*\\.py'", Uri.parse(document.uri).fsPath], document, cancellation);
     // All messages in pep8 are treated as warnings for now.
     messages.forEach((msg) => {
       msg.severity = LintMessageSeverity.Warning;
