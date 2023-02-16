cca5b6448fad33ebd543dca24d58c61dab9e0c30
Heyward Fann <fannheyward@gmail.com>
2023-02-16 11:24:03 +0800

    feat: diff.mjs

--- diff.mjs
+++ diff.mjs
@@ -1,12 +1,12 @@
-const fs = require('fs');
-const util = require('util');
+import { readFile } from 'fs';
+import { promisify } from 'util';
 
 async function diff() {
-  const text = await util.promisify(fs.readFile)('./package.json');
+  const text = await promisify(readFile)('./package.json');
   const config = JSON.parse(text);
   const overrides = config.contributes.configuration.properties['python.analysis.diagnosticSeverityOverrides'].properties;
 
-  const schemaText = await util.promisify(fs.readFile)('./schemas/pyrightconfig.schema.json');
+  const schemaText = await promisify(readFile)('./schemas/pyrightconfig.schema.json');
   const schema = JSON.parse(schemaText);
   for (const [key, val] of Object.entries(schema.properties)) {
     if (val['$ref'] === '#/definitions/diagnostic') {
@@ -22,4 +22,4 @@ async function diff() {
   }
 }
 
-diff();
+await diff();
