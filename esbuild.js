86c882d50499e0443fcb93c69ba148dddbab9d52
Heyward Fann <fannheyward@gmail.com>
2023-03-01 17:38:07 +0800

    fix(esbuild): rm watch mode

--- esbuild.js
+++ esbuild.js
@@ -1,9 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-async function start(watch) {
+async function start() {
   await require('esbuild').build({
     entryPoints: ['src/index.ts'],
     bundle: true,
-    watch,
     minify: process.env.NODE_ENV === 'production',
     sourcemap: process.env.NODE_ENV === 'development',
     mainFields: ['module', 'main'],
@@ -14,20 +13,6 @@ async function start(watch) {
   });
 }
 
-let watch = false;
-if (process.argv.length > 2 && process.argv[2] === '--watch') {
-  console.log('watching...');
-  watch = {
-    onRebuild(error) {
-      if (error) {
-        console.error('watch build failed:', error);
-      } else {
-        console.log('watch build succeeded');
-      }
-    },
-  };
-}
-
-start(watch).catch((e) => {
+start().catch((e) => {
   console.error(e);
 });
